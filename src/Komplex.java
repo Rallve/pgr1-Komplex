6153aef6bedf53a288d93f42e132c4a17b85a6d8
Rallve <77719378+Rallve@users.noreply.github.com>
2022-03-23 08:53:03 +0100

    created

--- src/Komplex.java
+++ src/Komplex.java
@@ -1,5 +1,18 @@
-public class Komplex {
-    public static void main(String[] args) {
+import java.util.Objects;
 
+public class Komplex {
+    double real;
+    double img;
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Komplex komplex = (Komplex) object;
+        return java.lang.Double.compare(komplex.real, real) == 0 && java.lang.Double.compare(komplex.img, img) == 0;
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), real, img);
     }
 }
